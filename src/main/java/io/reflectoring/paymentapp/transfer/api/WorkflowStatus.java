df0ae3c13ae71a3c498655b86d26e00ade27efb0
Tom Hombergs <tom.hombergs@gmail.com>
2023-12-17 12:30:01 +1100

    compiling version with Kalix (not tested, yet)

--- src/main/java/io/reflectoring/paymentapp/transfer/api/WorkflowStatus.java
+++ src/main/java/io/reflectoring/paymentapp/transfer/api/WorkflowStatus.java
@@ -1,22 +1,12 @@
 package io.reflectoring.paymentapp.transfer.api;
 
 public enum WorkflowStatus {
-    NOT_STARTED(Transfer.TransferWorkflowAction.FRAUD_CHECK),
-    WAITING_FOR_FRAUD_CHECK(Transfer.TransferWorkflowAction.FRAUD_CHECK),
-    FRAUD_CHECKED(Transfer.TransferWorkflowAction.DEBIT_SOURCE_ACCOUNT),
-    WAITING_FOR_DEBIT(Transfer.TransferWorkflowAction.DEBIT_SOURCE_ACCOUNT),
-    SOURCE_ACCOUNT_DEBITED(Transfer.TransferWorkflowAction.CREDIT_TARGET_ACCOUNT),
-    WAITING_FOR_CREDIT(Transfer.TransferWorkflowAction.CREDIT_TARGET_ACCOUNT),
-    COMPLETE(Transfer.TransferWorkflowAction.NONE),
-    FAILED(Transfer.TransferWorkflowAction.NONE);
-
-    private final Transfer.TransferWorkflowAction nextAction;
-
-    WorkflowStatus(Transfer.TransferWorkflowAction nextAction) {
-        this.nextAction = nextAction;
-    }
-
-    public Transfer.TransferWorkflowAction getNextAction() {
-        return nextAction;
-    }
+    NOT_STARTED,
+    WAITING_FOR_FRAUD_CHECK,
+    FRAUD_CHECKED,
+    WAITING_FOR_DEBIT,
+    SOURCE_ACCOUNT_DEBITED,
+    WAITING_FOR_CREDIT,
+    COMPLETE,
+    FAILED
 }
