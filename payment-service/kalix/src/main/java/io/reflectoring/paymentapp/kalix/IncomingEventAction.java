01af2f86c50f63be55e4ac8c8f9b4d9201d3d514
Tom Hombergs <tom.hombergs@gmail.com>
2024-03-06 10:57:37 +1100

    upgrading to Kalix SDK 1.3.8

--- payment-service/kalix/src/main/java/io/reflectoring/paymentapp/kalix/IncomingEventAction.java
+++ payment-service/kalix/src/main/java/io/reflectoring/paymentapp/kalix/IncomingEventAction.java
@@ -14,40 +14,40 @@ import org.slf4j.LoggerFactory;
 
 public class IncomingEventAction extends Action {
 
-//    private static final Logger logger = LoggerFactory.getLogger(IncomingEventAction.class);
-//
-//    @Subscribe.Topic(Topics.REQUEST_FRAUD_CHECK)
-//    @Publish.Topic(Topics.FRAUD_CHECKED)
-//    public Effect<FraudCheckedEvent> onRequestFraudCheckEvent(RequestFraudCheckEvent event) {
-//        logger.info("received event {} via topic", event);
-//        return effects().reply(new FraudCheckedEvent(
-//                event.transferId(),
-//                FraudCheckedEvent.FraudCheckResult.SUCCESS
-//        ));
-//    }
-//
-//    @Subscribe.Topic(Topics.REQUEST_ACCOUNT_DEBIT)
-//    @Publish.Topic(Topics.ACCOUNT_DEBITED)
-//    public Effect<AccountDebitedEvent> onRequestFraudCheckEvent(RequestAccountDebitEvent event) {
-//        logger.info("received event {} via topic", event);
-//        return effects().reply(new AccountDebitedEvent(
-//                event.transferId(),
-//                event.sourceAccountId(),
-//                event.amount(),
-//                AccountDebitedEvent.DebitResult.SUCCESS
-//        ));
-//    }
-//
-//    @Subscribe.Topic(Topics.REQUEST_ACCOUNT_CREDIT)
-//    @Publish.Topic(Topics.ACCOUNT_CREDITED)
-//    public Effect<AccountCreditedEvent> onRequestFraudCheckEvent(RequestAccountCreditEvent event) {
-//        logger.info("received event {} via topic", event);
-//        return effects().reply(new AccountCreditedEvent(
-//                event.transferId(),
-//                event.targetAccountId(),
-//                event.amount(),
-//                AccountCreditedEvent.CreditResult.SUCCESS
-//        ));
-//    }
+    private static final Logger logger = LoggerFactory.getLogger(IncomingEventAction.class);
+
+    @Subscribe.Topic(Topics.REQUEST_FRAUD_CHECK)
+    @Publish.Topic(Topics.FRAUD_CHECKED)
+    public Effect<FraudCheckedEvent> onRequestFraudCheckEvent(RequestFraudCheckEvent event) {
+        logger.info("received event {} via topic", event);
+        return effects().reply(new FraudCheckedEvent(
+                event.transferId(),
+                FraudCheckedEvent.FraudCheckResult.SUCCESS
+        ));
+    }
+
+    @Subscribe.Topic(Topics.REQUEST_ACCOUNT_DEBIT)
+    @Publish.Topic(Topics.ACCOUNT_DEBITED)
+    public Effect<AccountDebitedEvent> onRequestFraudCheckEvent(RequestAccountDebitEvent event) {
+        logger.info("received event {} via topic", event);
+        return effects().reply(new AccountDebitedEvent(
+                event.transferId(),
+                event.sourceAccountId(),
+                event.amount(),
+                AccountDebitedEvent.DebitResult.SUCCESS
+        ));
+    }
+
+    @Subscribe.Topic(Topics.REQUEST_ACCOUNT_CREDIT)
+    @Publish.Topic(Topics.ACCOUNT_CREDITED)
+    public Effect<AccountCreditedEvent> onRequestFraudCheckEvent(RequestAccountCreditEvent event) {
+        logger.info("received event {} via topic", event);
+        return effects().reply(new AccountCreditedEvent(
+                event.transferId(),
+                event.targetAccountId(),
+                event.amount(),
+                AccountCreditedEvent.CreditResult.SUCCESS
+        ));
+    }
 
 }
